a60ba59418930d011e514dd4da7c09d6ea248d6d
陈克锋 <chenkefeng@kuaicto.com>
2017-07-13 16:50:56 +0800

    fix bug

--- SDMenuBarViewKit/Classes/NSObject+RemoveKVO.m
+++ SDMenuBarViewKit/Classes/NSObject+RemoveKVO.m
@@ -7,7 +7,25 @@
 //
 
 #import "NSObject+RemoveKVO.h"
+#import <objc/message.h>
+
 
 @implementation NSObject (RemoveKVO)
+
++ (void)load {
+    Method m1 = class_getInstanceMethod([self class], @selector(sd_removeObserver:forKeyPath:));
+    Method m2 = class_getInstanceMethod([self class], @selector(removeObserver:forKeyPath:));
+    method_exchangeImplementations(m1, m2);
+}
+
+- (void)sd_removeObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath {
+    @try {
+        [self sd_removeObserver:observer forKeyPath:keyPath];
+    } @catch (NSException *exception) {
+        
+    } @finally {
+        
+    }
+}
 
 @end
